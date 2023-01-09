e03da3324584b13f9f4069ce134599b9f49d43a0
e1b20069 <e1b20069@st.oit.ac.jp>
2023-01-09 10:16:47 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/z1661/poker/poker/PokerApplication.java
@@ -0,0 +1,13 @@
+package oit.is.z1661.poker.poker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/z1661/poker/poker/PokerApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.z1661.poker.poker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
